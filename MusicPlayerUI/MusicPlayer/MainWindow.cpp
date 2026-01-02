49e0da3aeae0eaece53fe5f0edf730dc4f87a61c
jaemyeong311 <jaemyoeng311@gmail.com>
2026-01-02 23:52:14 +0900

    111

--- MusicPlayerUI/MusicPlayer/MainWindow.cpp
+++ MusicPlayerUI/MusicPlayer/MainWindow.cpp
@@ -1,11 +1,55 @@
-#include "MainWindow.h"
+﻿//#include "MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
+//MainWindow::MainWindow(QWidget *parent)
+//    : QMainWindow(parent)
+//{
+//    ui.setupUi(this);
+//}
+
+//MainWindow::~MainWindow()
+//{}
+
+
+
+
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
 {
-    ui.setupUi(this);
+    ui->setupUi(this);
+
+    // 오디오 출력 & 플레이어 생성
+    audioOutput = new QAudioOutput(this);
+    player = new QMediaPlayer(this);
+    player->setAudioOutput(audioOutput);
+
+    // 음악 파일 지정 (테스트용)
+    player->setSource(QUrl::fromLocalFile(
+        "C:\Users\jaemy\Downloads\test.mp3"   // ⚠️ 본인 PC 경로로 수정
+    ));
+
+    // 버튼 클릭 시그널 연결
+    connect(ui->btnPlay, &QPushButton::clicked,
+        this, &MainWindow::onPlayClicked);
+
+    connect(ui->btnPause, &QPushButton::clicked,
+        this, &MainWindow::onPauseClicked);
 }
 
 MainWindow::~MainWindow()
-{}
+{
+    delete ui;
+}
 
+void MainWindow::onPlayClicked()
+{
+    player->play();
+}
+
+void MainWindow::onPauseClicked()
+{
+    player->pause();
+}
--- MusicPlayerUI/MusicPlayer/MainWindow.h
+++ MusicPlayerUI/MusicPlayer/MainWindow.h
@@ -1,17 +1,47 @@
+//#pragma once
+
+//#include <QtWidgets/QMainWindow>
+//#include "ui_MainWindow.h"
+
+//class MainWindow : public QMainWindow
+//{
+//    Q_OBJECT
+//
+//public:
+//    MainWindow(QWidget *parent = nullptr);
+//    ~MainWindow();
+//
+//private:
+//    Ui::MainWindowClass ui;
+//};
+
+
+
 #pragma once
 
-#include <QtWidgets/QMainWindow>
-#include "ui_MainWindow.h"
+#include <QMainWindow>
+#include <QMediaPlayer>
+#include <QAudioOutput>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
-private:
-    Ui::MainWindowClass ui;
-};
+private slots:
+    void onPlayClicked();
+    void onPauseClicked();
 
+private:
+    Ui::MainWindow* ui;
+
+    QMediaPlayer* player;
+    QAudioOutput* audioOutput;
+};
