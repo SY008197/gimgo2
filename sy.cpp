b730c22548a3df0ae7ed4667d08e683aee86f579
SY008197 <sangy1012@gmail.com>
2026-01-03 18:25:03 +0900

    파일 경로 추가

--- sy.cpp
+++ sy.cpp
@@ -37,26 +37,29 @@ using namespace std; -> using std::string; 로 변경
 #include <string>
 #include <vector>
 using std::string;                  // string 타입 사용(namespace보다 이런식으로 습관화하기)
+using std::vector;                  // vector 타입 사용
 class Library
 {
-private:                            // 노래에 대한 정보는 라이브러리 클래스에서만 접근
-    struct Song                        // 노래를 구성하는 요소를 묶기 위한 구조체 선언
-    {
-        string S_title;                // 제목
-        string S_singer;            // 가수
-    };
-    vector<Song> SongList;            // 노래를 담을 컨테이너
+    private:                            // 노래에 대한 정보는 라이브러리 클래스에서만 접근
+        struct Song                        // 노래를 구성하는 요소를 묶기 위한 구조체 선언
+        {
+            string S_title;                // 제목
+            string S_singer;            // 가수
+		    string S_filePath;            // 파일 경로
+        };
+        vector<Song> SongList;            // 노래를 담을 컨테이너
 
-    void addSong(const string& title, const string& singer) // 멤버 함수 선언(노래추가)
-
-    vector<Song> searchByTitle(const string& title)                // 멤버 함수 선언(제목 검색)
+    public:
+        void addSong(const string& title, const string& singer, const string& filePath); // 멤버 함수 선언(노래추가)
+        vector<Song> searchByTitle(const string& title);                // 멤버 함수 선언(제목 검색)
 };
 //Library 클래스의 멤버 함수 정의(노래 추가)
-void Library::addSong(const string& title, const string& singer)  // 매개변수로 노래제목과 가수명을 받음(이때 값은 상수로 고정시키고 참조만 한다)
+void Library::addSong(const string& title, const string& singer, const string& filePath)  // 매개변수로 노래제목, 가수명, 파일 경로를 받음(이때 값은 상수로 고정시키고 참조만 한다)
 {
     Song newSong;                    // 새로운 노래 구조체 생성
     newSong.S_title = title;        // 제목 설정
     newSong.S_singer = singer;    // 가수 설정
+    newSong.S_filePath = filePath;        // 파일 경로 설정
     SongList.push_back(newSong);    // 컨테이너에 노래 추가
 }   
 
