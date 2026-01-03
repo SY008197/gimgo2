9e2c65b20e54d3e7a7ce73eda10dc8e8b6dbc4ad
SY008197 <sangy1012@gmail.com>
2026-01-03 19:45:22 +0900

    코드 분리

--- sy.cpp
+++ sy.cpp
@@ -1,59 +1,8 @@
-//Day1_상영
-/*
-* 목표설계
-저장된 노래
-검색 (제목 or 가수명으로 찾기)
-전체 목록
-
-재생목록
-저장된 재생 목록이 있으면 출력
-없으면 "저장된 재생목록이 없습니다."같은 안내문 출력
-
-재생목록 추가
-이름설정
-추가하기
-저장하기
-
-프로그램 종료
-*/
-
-//Day2_상영
-/*
-라이브러리 클래스 기본 틀 구축
-라이브러리 클래스 내부에 노래 구조체 설계
-벡터를 이용해 노래를 담을 컨테이너 생성
-*/
-
-//Day3_상영
-/*
-using namespace std; -> using std::string; 로 변경
-라이브러리 클래스 내부에 노래 추가 멤버 함수 선언
-라이브러리 클래스 외부에 노래 추가 멤버 함수 정의
-라이브러리 클래스 내부에 제목으로 검색하는 멤버 함수 선언
-라이브러리 클래스 외부에 제목으로 검색하는 멤버 함수 정의
-*/
-
 #include <iostream>
 #include <string>
 #include <vector>
 using std::string;                  // string 타입 사용(namespace보다 이런식으로 습관화하기)
 using std::vector;                  // vector 타입 사용
-class Library
-{
-    private:                            // 노래에 대한 정보는 라이브러리 클래스에서만 접근
-        struct Song                        // 노래를 구성하는 요소를 묶기 위한 구조체 선언
-        {
-            string S_title;                // 제목
-            string S_singer;            // 가수
-		    string S_filePath;            // 파일 경로
-        };
-        vector<Song> SongList;            // 노래를 담을 컨테이너
-
-    public:
-        void addSong(const string& title, const string& singer, const string& filePath); // 멤버 함수 선언(노래추가)
-        vector<Song> searchByTitle(const string& title);                // 멤버 함수 선언(제목 검색)
-};
-//Library 클래스의 멤버 함수 정의(노래 추가)
 void Library::addSong(const string& title, const string& singer, const string& filePath)  // 매개변수로 노래제목, 가수명, 파일 경로를 받음(이때 값은 상수로 고정시키고 참조만 한다)
 {
     Song newSong;                    // 새로운 노래 구조체 생성
