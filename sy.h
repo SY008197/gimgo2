9e9feef9246670aa4d4307e4277f8c8edd5be776
SY008197 <sangy1012@gmail.com>
2026-01-03 20:05:43 +0900

    코드분리 헤더파일

--- sy.h
+++ sy.h
@@ -3,21 +3,21 @@
 #include <string>
 #include <vector>
 
-using std::string;                  // string 타입 사용(namespace보다 이런식으로 습관화하기)
-using std::vector;                  // vector 타입 사용
+// using std::string;과 같은 네임스페이스 지시문은 헤더 파일에 포함하지 않는 것이 좋음
+
 class Library
 {
 private:                            // 노래에 대한 정보는 라이브러리 클래스에서만 접근
     struct Song                        // 노래를 구성하는 요소를 묶기 위한 구조체 선언
     {
-        string S_title;                // 제목
-        string S_singer;            // 가수
-        string S_filePath;            // 파일 경로
+        std::string S_title;                // 제목
+        std::string S_singer;            // 가수
+        std::string S_filePath;            // 파일 경로
     };
-    vector<Song> SongList;            // 노래를 담을 컨테이너
+    std::vector<Song> SongList;            // 노래를 담을 컨테이너
 
 public:
-    void addSong(const string& title, const string& singer, const string& filePath); // 멤버 함수 선언(노래추가)
-    vector<Song> searchByTitle(const string& title);                // 멤버 함수 선언(제목 검색)
+    void addSong(const std::string& title, const std::string& singer, const std::string& filePath); // 멤버 함수 선언(노래추가)
+    std::vector<Song> searchByTitle(const std::string& title);                // 멤버 함수 선언(제목 검색)
 };
 
